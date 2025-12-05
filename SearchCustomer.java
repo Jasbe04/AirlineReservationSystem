7bb5088ed65cdf2b09e71263bfe53f74e0df1852
Tahmid Hossain Jasbe <tahmidhossainjasbe@gmail.com>
2025-12-06 01:56:13 +0600

    Update SearchCustomer.java

--- SearchCustomer.java
+++ SearchCustomer.java
@@ -388,7 +388,7 @@ public class SearchCustomer extends javax.swing.JInternalFrame {
             PreparedStatement pre;
             
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/airline_project", "root", "j@sbe04");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/airline_project", "root", "your_password_here");
             
             pre=con.prepareStatement("select * from customer where CustomerID=? ");
             
